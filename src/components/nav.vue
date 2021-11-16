41164a09996759bdb34a510ad4cbf57838e6ddc3
unknown <965963765@qq.com>
2021-11-17 00:12:53 +0800

    'push'

---
+++ src/components/nav.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="nav-wrap">
+        <div class="nav-left">
+            <div class="pic-wrap">
+                <img src="">
+                <span class="pic-name">家威</span>
+            </div>
+        </div>
+        <div class="nav-right">
+            <ul>
+                <li>首页</li>
+                <li>学习</li>
+                <li>文章</li>
+                <li>小工具</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    #nav-wrap{
+        display: flex;
+    }
+    ul{
+        display: flex;
+    }
+    li{
+        list-style: none;
+    }
+</style>
